8a0420e66ca779fe0420d16b2fd98015fa5f88af
jzyang <2634031+jzyang@users.noreply.github.com>
2021-12-21 22:18:56 -0800

    Update day14.py
    Both solutions

--- day14.py
+++ day14.py
@@ -16,61 +16,74 @@ def parse_file():
     
     return template, pairs, dict
 
-def insert_by_step(template, pairs, dict, total_cnt):
+def insert_by_step(pairs, total_cnt, dict):
     new_pairs = []
-    new_template = ''
-    for index in range(len(template) - 1):
-        pair = template[index:index+2]
+    delete_pairs = []
+    for pair in pairs:
         insert_val = dict[pair]
-        add_to_dict(total_cnt, insert_val)
-        if index == 0:
-            new_template += pair[0] + insert_val + pair[1]
-        else:
-            new_template += insert_val + pair[1]
             
-    for index in range(len(new_template) - 1):
-        pair = new_template[index:index+2]
-        add_to_dict(pairs, pair)
+        pair1 = pair[0] + insert_val
+        pair2 = insert_val + pair[1]
+        cnt = pairs[pair]
+        add_to_dict(total_cnt, insert_val, cnt)
+        
+        # This pair is no longer needed
+        delete_pairs.append(pair)
+        
+        # add new pairs
+        new_pairs.append((pair1, cnt))
+        new_pairs.append((pair2, cnt))
+            
+    delete(pairs, delete_pairs)
     
-    return new_template
-
-def count_letters(template):
-    total_cnt = {}
-    for letter in template:
-        add_to_dict(total_cnt, letter)
-    
-    return total_cnt
+    for val in new_pairs:
+        pair = val[0]
+        cnt = val[1]
+        add_to_dict(pairs, pair, cnt)
 
 def diff_max_min(total_cnt):
-    min = 999
-    max = 0
+    min = -1
+    max = -1
+    print(total_cnt)
     for key in total_cnt.keys():
         val = total_cnt[key]
-        if val < min:
+        if min == -1 or val < min:
             min = val
         elif val > max:
             max = val
     
     return max - min
-
-def add_to_dict(dict, key):
+        
+def add_to_dict(dict, key, val):
     if key in dict.keys():
-        dict[key] += 1
+        dict[key] += val
     else:
-        dict[key] = 1
+        dict[key] = val
+
+def delete(dict, keys):
+    for key in keys:
+        dict.pop(key)
+
+def count(line):
+    letters = {}
+    for letter in line:
+        add_to_dict(letters, letter, 1)
+    return letters
+
+def sum_letters(total_cnt):
+    total_letters = 0
+    for letter in total_cnt.keys():
+        total_letters += total_cnt[letter]
+    return total_letters
 
 def get_day_14_solutions():
     template, pairs, dict  = parse_file()
-    print(template)
-    new_template = template
-    total_cnt = count_letters(new_template)
-    print(pairs)
-    print(total_cnt)
-    for i in range(40):
-        new_template = insert_by_step(new_template, pairs, dict, total_cnt)
-        if i == 9:
-            print(diff_max_min(total_cnt))
-            break
-    print(diff_max_min(total_cnt))
+    total_cnt = count(template)
+    for i in range(10):
+        insert_by_step(pairs, total_cnt, dict)
+    print("step 10: %d" % diff_max_min(total_cnt))
+    for i in range(30):
+        insert_by_step(pairs, total_cnt, dict)
+    print("step 30: %d" % diff_max_min(total_cnt))
 
 get_day_14_solutions()
